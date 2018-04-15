54a1dbc3063683ee946ee98a72aa2c8726b3adcd
Adam Pawliński <adam.pawlinski@gmail.com>
2018-04-15 06:22:15 +0200

    switch conditional out of if

--- index.js
+++ index.js
@@ -3,26 +3,27 @@ var OSinfo = require('./modules/OSinfo');
 
 process.stdin.on('readable', function() {
   var input = process.stdin.read();
-  if (input !== null) {
-    var instruction = input.toString().trim();
-    switch (instruction) {
-      case '/exit':
-        process.stdout.write('Quitting app!\n');
-        process.exit();
-        break;
-      case '/node':
-        node = process.versions.node;
-        process.stdout.write(node + '\n');
-        break;
-      case '/lang':
-        lang = process.env.lang;
-        process.stdout.write(lang + '\n');
-        break;
-      case '/getOSinfo':
-        OSinfo.print();
-        break;
-      default:
-        process.stderr.write('Wrong instruction!\n');
-    }
+  if (input === null) {
+    return
+  }
+  var instruction = input.toString().trim();
+  switch (instruction) {
+    case '/exit':
+      process.stdout.write('Quitting app!\n');
+      process.exit();
+      break;
+    case '/node':
+      node = process.versions.node;
+      process.stdout.write(node + '\n');
+      break;
+    case '/lang':
+      lang = process.env.lang;
+      process.stdout.write(lang + '\n');
+      break;
+    case '/getOSinfo':
+      OSinfo.print();
+      break;
+    default:
+      process.stderr.write('Wrong instruction!\n');
   }
 });
